a27ef7819388c113dff153cdf419c6c521e68dc5
Pun VireakRoth <vireakrothpun@gmail.com>
2025-03-18 22:48:25 +0700

    ⚙️Necessary dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
 }
 
 dependencies {
-
+    // Core dependencies
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
@@ -44,4 +44,21 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+
+    // Retrofit for network requests
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // OkHttp for HTTP client
+    implementation ("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.11.0")
+
+    // Glide for image loading
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+
+    // Location services
+    implementation ("com.google.android.gms:play-services-location:21.0.1")
+
+    // Chart library for AQI visualization
+    implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
 }
