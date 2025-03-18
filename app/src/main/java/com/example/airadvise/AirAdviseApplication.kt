ebd350d5e1bab89df6ac0cf897b0974b47ec31b5
Pun VireakRoth <vireakrothpun@gmail.com>
2025-03-18 23:11:02 +0700

    ⚙️Initialize permissions, colours, base app class

---
+++ app/src/main/java/com/example/airadvise/AirAdviseApplication.kt
@@ -0,0 +1,11 @@
+package com.example.airadvise
+
+import android.app.Application
+
+class AirAdviseApplication : Application() {
+    override fun onCreate()
+    {
+        super.onCreate()
+        // use to initialize any app libraries or configurations
+    }
+}
