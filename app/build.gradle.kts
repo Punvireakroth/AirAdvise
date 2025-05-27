7832756e035ae275df83d23f6b754418159b8465
Pun VireakRoth <vireakrothpun@gmail.com>
2025-05-27 23:48:08 +0700

    Map Fragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
+    id("kotlin-kapt")
 }
 
 android {
@@ -46,6 +48,7 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+    implementation("com.google.android.gms:play-services-maps:19.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
@@ -60,7 +63,7 @@ dependencies {
 
     // Glide for image loading
     implementation ("com.github.bumptech.glide:glide:4.16.0")
-
+    kapt ("com.github.bumptech.glide:compiler:4.16.0")
     // Location services
     implementation ("com.google.android.gms:play-services-location:21.0.1")
 
@@ -77,7 +80,22 @@ dependencies {
 
     // Material UI
     implementation("com.google.android.material:material:1.12.0")
-
     implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
 
+    // Google Maps
+    implementation ("com.google.android.gms:play-services-maps:19.2.0")
+    implementation ("com.google.android.gms:play-services-location:21.0.1")
+
+    // Room
+    implementation("androidx.room:room-runtime:2.5.2")
+    implementation("androidx.room:room-ktx:2.5.2")
+    kapt("androidx.room:room-compiler:2.5.2")
+
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.6.4")
+
+    // Lifecycle
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
 }
